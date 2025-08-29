a46f192bb3da04ffd639a1996ac0b4c4802ee961
ishani-k <ishanikundu.ik@gmail.com>
2025-08-30 00:05:36 +0530

    temp

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,24 +1,39 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
+import Link from 'next/link'
+import { Button } from './ui/button'
 
 const Header = () => {
   return (
-    <div className='fixed top-0'>
-
-      <nav>
+    <div className='fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b'>
+      <nav className='container mx-auto px-4 py-0 flex items-center justify-between'>
         <Link href="/">
-        <Image />
+        <Image src={"/logo.png"} alt='Prospr logo' height={60} width={200}
+        className='h-15 w-auto object-contain'/>
         </Link>
-      </nav>
 
 
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </div>
+      
+        <SignedIn>
+          <Link href={"/dashboard"}>
+          <Button variant="outline">
+            
+
+          </Button>
+          </Link>
+        </SignedIn>
+
+        <SignedOut>
+          <SignInButton forceRedirectUrl="/dashboard">
+            <Button variant="outline">Login</Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        </nav>
+      </div>
+    
   )
 }
 
