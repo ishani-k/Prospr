c353bf00bd7f0d0e698a8500ae53762edde2045a
ishani-k <ishanikundu.ik@gmail.com>
2025-09-01 17:46:29 +0530

    developing landing page

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import HeroSection from "@/components/hero";
 
 export default function Home() {
   return (
-    <div className="mt-40">
+    <div className="mt-30">
       <HeroSection/>
     </div>
 
--- components/hero.jsx
+++ components/hero.jsx
@@ -2,36 +2,71 @@
 
 import Link from "next/link";
 import { Button } from "./ui/button";
+import Image from "next/image";
+import { useEffect, useRef } from "react";
 
 const HeroSection = () => {
+
+
+  const imageRef = useRef()
+
+  useEffect(() =>{
+
+    const imageElement = imageRef.current;
+
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const scrollThreshold = 100;
+
+      if(scrollPosition > scrollThreshold)
+      {
+        imageElement.classList.add("scrolled")
+      }
+      else
+      {
+        imageElement.classList.remove("scrolled")
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll)
+
+    return () => window.removeEventListener("scroll", handleScroll)
+
+  }, [])
+
   return (
     <div className="pb-20 px-4">
-      <div>
-        <h1>
+      <div className="container mx-auto text-center">
+        <h1 className="text-5xl md:text-8xl lg:text-[70px] tracking-tighter pb-6 bg-gradient-to-br from-[#bbd2cf] via-[#536976] to-[#292e49] text-transparent bg-clip-text">
           Money made simple. <br />
           Insights made smart.
         </h1>
-        <p>
+        <p className="text-xl text-gray-600 mb-8 max-w-2xl mx-auto">
           Your all-in-one AI-powered finance companion that helps you manage
           today and prepare for tomorrow.
         </p>
 
-        <div>
+        <div className="flex justify-center space-x-4">
           <Link href="/dashboard">
             <Button size="lg" className="px-8">
               Get Started
             </Button>
           </Link>
-        </div>
-        <div>
           <Link href="/dashboard">
             <Button variant="outline" size="lg" className="px-8">
               Watch Demo
             </Button>
           </Link>
         </div>
-        <div>
-          <div></div>
+        <div className="hero-image-wrapper">
+          <div ref={imageRef} className="hero-image">
+            <Image src="/DSC_9800.jpg"
+            width={1280}
+            height={720}
+            alt="dashboard preview"
+            className="rounded-lg shadow-2xl border mx-auto"
+            priority />
+          </div>
         </div>
       </div>
     </div>
