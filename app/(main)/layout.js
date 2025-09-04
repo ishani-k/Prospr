112459d9b1aadf64a1381c6ba58375166dbcc017
ishani-k <ishanikundu.ik@gmail.com>
2025-09-04 23:28:44 +0530

    temp

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -1,9 +1,19 @@
-import React from 'react'
+/*import React from 'react'
 
 const MainLayout = ({children}) => {
-  return (
-    <div className='container mx-auto my-32'>{children}</div>
-  )
+  return <div className='container mx-auto my-32'>{children}</div>
+
 }
 
-export default MainLayout
+export default MainLayout */
+
+import { checkUser } from "@/lib/checkUser";
+
+export default async function MainLayout({ children }) {
+  await checkUser(); // runs server-side only ✅
+  return (
+    <div className="container mx-auto my-32">
+      {children}
+    </div>
+  );
+}
