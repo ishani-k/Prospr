38e1ef846875f3934611c06f9145969a18708390
ishani-k <ishanikundu.ik@gmail.com>
2025-09-08 01:21:47 +0530

    temp

--- components/create-account-drawer.jsx
+++ components/create-account-drawer.jsx
@@ -5,12 +5,22 @@ import { Button } from './ui/button';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { accountSchema } from '@/app/lib/schema';
+import { Input } from './ui/input';
+import { Radio, RadioIcon, RadioReceiver, RadioTowerIcon } from 'lucide-react';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './ui/select';
 
 const CreateAccountDrawer = ({children}) => {
 
     const [open, setOpen] = useState(false)
 
-    const {register, handleSubmit, formState: { errors }, setValue, watch, } = useForm({
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        setValue,
+        watch, 
+        reset
+    } = useForm({
         resolver: zodResolver(accountSchema),
         defaultValues: {
             name: "",
@@ -28,8 +38,29 @@ const CreateAccountDrawer = ({children}) => {
                 <DrawerTitle>Create New Account</DrawerTitle>
             </DrawerHeader>
 
-            <div>
-                <form></form>
+            <div className='px-4 pb-4'>
+                <form className='space-y-4'>
+                    <div className='space-y-2'>
+                        <label htmlFor="name" className='text-sm font-medium'>Account Name</label>
+                        <Input id="name" placeholder="e.g, Main Checking" 
+                        {...register("name")} />
+                        {errors.name && (
+                            <p className='text-sm text-red-700'>{errors.name.message}</p>
+                        )}
+                    </div>
+                    <div className='space-y-2'>
+                        <label htmlFor="type" className='text-sm font-medium'>Account Type</label>
+                        <Select>
+                          <SelectTrigger id="type">
+                            <SelectValue placeholder="Select Type" />
+                          </SelectTrigger>
+                          <SelectContent>
+                            <SelectItem value="CURRENT">Current</SelectItem>
+                            <SelectItem value="SAVINGS">Savings</SelectItem>
+                          </SelectContent>
+                        </Select>
+                    </div>
+                </form>
             </div>
         </DrawerContent>
     </Drawer>
