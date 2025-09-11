5138ebd0f7cb17924fb8e1b518fd3aefb1afc926
ishani-k <ishanikundu.ik@gmail.com>
2025-09-11 20:52:50 +0530

    temp

--- actions/dashboard.js
+++ actions/dashboard.js
@@ -72,3 +72,23 @@ export async function createAccount(data) {
         throw new Error(error.message)
     }
 }
+
+
+export async function getUserAccounts() {
+    const {userId} = await auth()
+
+    if(!userId) throw new Error("Unauthorized")
+
+    const user = await db.user.findUnique({
+        where: { clerkUserId: userId },
+    })
+
+    if(!user)
+    {
+         throw new Error("User not found")
+    }
+
+    
+
+
+}
