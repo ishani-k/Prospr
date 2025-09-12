ac4f539d417c51ec8a674c5f29b61b9ff6738728
ishani-k <ishanikundu.ik@gmail.com>
2025-09-12 22:02:22 +0530

    temp

--- app/(main)/dashboard/_components/accountCard.jsx
+++ app/(main)/dashboard/_components/accountCard.jsx
@@ -1,8 +1,30 @@
+import { Card, CardAction, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { Switch } from '@/components/ui/switch'
 import React from 'react'
 
-const AccountCard = () => {
+const AccountCard = ({ account }) => {
+
+  const { name, type, balance, id, isDefault} = account
+
+
   return (
-    <div>AccountCard</div>
+    <Card>
+      <CardHeader>
+        <CardTitle>{name}</CardTitle>
+        <Switch />
+      </CardHeader>
+      <CardContent>
+       <div className='text-2xl font-bold'>
+        ${parseFloat(balance).toFixed(2)}
+       </div>
+       <p className='text-xs text-muted-foreground'>
+        {type.charAt(0) + type.slice(1).toLowerCase()} Account
+       </p>
+      </CardContent>
+      <CardFooter>
+        <p>Card Footer</p>
+      </CardFooter>
+    </Card>
   )
 }
 
