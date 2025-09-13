5cd12b2568b8f8b0bef86e83789bf5ee071aefb7
ishani-k <ishanikundu.ik@gmail.com>
2025-09-13 20:10:59 +0530

    temp

--- app/(main)/account/[id]/page.jsx
+++ app/(main)/account/[id]/page.jsx
@@ -13,15 +13,15 @@ const AccountsPage = async ({params}) => {
     const { transactions, ...account } = accountData
 
   return (
-    <div>
+    <div className='space-y-8 px-5 flex gap-4 items-end justify-between'>
         <div>
-            <h2>{account.name}</h2>
-            <p>{account.type.charAt(0) + account.type.slice(1).toLowerCase()} Account</p>
+            <h1 className='text-5xl sm:text-6xl tracking-tight bg-gradient-to-b from-blue-500 to-blue-900 text-transparent bg-clip-text capitalize'>{account.name}</h1>
+            <p className='text-muted-foreground'>{account.type.charAt(0) + account.type.slice(1).toLowerCase()} Account</p>
         </div>
 
-        <div>
-            <div>₹{parseFloat(account.balance).toFixed(2)}</div>
-            <p>{account._count.transactions} Transactions</p>
+        <div className='text-right pb-2'>
+            <div className='text-xl sm:text-2xl font-bold'>₹{parseFloat(account.balance).toFixed(2)}</div>
+            <p className='text-sm text-muted-foreground'>{account._count.transactions} Transactions</p>
         </div>
 
         {/* Chart */}
