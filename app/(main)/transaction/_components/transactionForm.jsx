8fb22fa7a2436ef564f9a68a6923ec67d5919a05
ishani-k <ishanikundu.ik@gmail.com>
2025-09-22 22:17:29 +0530

    add transactions page

---
+++ app/(main)/transaction/_components/transactionForm.jsx
@@ -0,0 +1,11 @@
+"use client"
+
+import React from 'react'
+
+const AddTransactionForm = () => {
+  return (
+    <div>AddTransactionForm</div>
+  )
+}
+
+export default AddTransactionForm
--- app/(main)/transaction/create/page.jsx
+++ app/(main)/transaction/create/page.jsx
@@ -1,7 +1,22 @@
+import { getUserAccounts } from '@/actions/dashboard'
+import { defaultCategories } from '@/data/categories'
 import React from 'react'
+import AddTransactionForm from '../_components/transactionForm'
 
-const Transaction = () => {
-  return <div>Transaction Page</div>
+const addTransactionPage = async () => {
+
+  const accounts = await getUserAccounts()
+
+  return (
+    <div className='max-w-3xl mx-auto px-5'>
+      <h1 className='text-5xl'>Add Transaction</h1>
+
+      <AddTransactionForm
+      accounts = {accounts}
+      categories = {defaultCategories}
+      />
+    </div>
+  )
 }
 
-export default Transaction
+export default addTransactionPage
