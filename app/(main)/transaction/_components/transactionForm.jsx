0b3f14928ec9caf790a117b2188bd13b74fd6f44
ishani-k <ishanikundu.ik@gmail.com>
2025-09-24 14:37:58 +0530

    add transaction func

--- app/(main)/transaction/_components/transactionForm.jsx
+++ app/(main)/transaction/_components/transactionForm.jsx
@@ -44,9 +44,15 @@ const AddTransactionForm = ({accounts, categories}) => {
   const isRecurring = watch("isRecurring")
   const date = watch("date")
 
+  const filteredCategories = categories.filter(
+    (category) => category.type === type
+  )
+
   return (
     <form className='space-y-6'>
       {/*AI Receipt Scanner */}
+
+      {/*Type(inc/exp) */}
       <div className='space-y-2'>
         <label className='text-sm font-medium'>Type</label>
         <Select onValueChange = {(value) => setValue("type", value)}
@@ -64,7 +70,9 @@ const AddTransactionForm = ({accounts, categories}) => {
         )}
       </div>
 
+
       <div className='grid gap-6 md:grid-cols-2'>
+        {/* Amount */}
         <div className='space-y-2'>
           <label className='text-sm font-medium pr-2'>Amount</label>
           <Input type="number" step='0.01' placeholder='0.00' {...register('amount')} className='w-full'/>
@@ -74,24 +82,35 @@ const AddTransactionForm = ({accounts, categories}) => {
           )}
         </div>
 
+        {/*Account */}
         <div>
           <label className='text-sm font-medium'>Account</label>
           <Select onValueChange = {(value) => setValue("accountId", value)}
             defaultValue={ getValues('accountId') }>
             <SelectTrigger className='w-full'>
-              <SelectValue placeholder="Select category" />
+              <SelectValue placeholder="Select account" />
             </SelectTrigger>
             <SelectContent>
-              {filteredCategories.map((category) => (
-                <SelectItem key={category.id} value={category.id}>
-                  {category.name}
+              {accounts.map((account) => (
+                <SelectItem key={account.id} value={account.id}>
+                  {account.name} (₹{parseFloat(account.balance).toFixed(2)})
                 </SelectItem>
               ))}
+              <CreateAccountDrawer>
+                <Button variant='ghost' className='w-full select-none items-center text-sm outline-none'>
+                  Create Account
+                </Button>
+              </CreateAccountDrawer>
             </SelectContent>
           </Select>
+
+          {errors.accountId && (
+          <p className='text-sm text-red-600'>{errors.accountId.message}</p>
+          )}
         </div>
       </div>
 
+      {/*Category */}
       <div>
         <label className='text-sm font-medium'>Category</label>
         <Select onValueChange = {(value) => setValue("category", value)}
@@ -100,14 +119,22 @@ const AddTransactionForm = ({accounts, categories}) => {
             <SelectValue placeholder="Select Category" />
           </SelectTrigger>
           <SelectContent>
-            <SelectItem value="EXPENSE">Expense</SelectItem>
-            <SelectItem value="INCOME">Income</SelectItem>
-          </SelectContent>
+              {filteredCategories.map((category) => (
+                <SelectItem key={category.id} value={category.id}>
+                  {category.name}
+                </SelectItem>
+              ))}
+            </SelectContent>
         </Select>
-        {errors.type && (
-          <p className='text-sm text-red-600'>{errors.type.message}</p>
+        {errors.category && (
+          <p className='text-sm text-red-600'>{errors.category.message}</p>
         )}
         
+      </div>
+
+      {/* Date */}
+      <div>
+        <label className='text-sm font-medium'>Date</label>
       </div>
      
     </form>
